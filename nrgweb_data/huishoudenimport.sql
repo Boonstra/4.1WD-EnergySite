071d02e41d54191c6b9ddc451e376eba6d1c73cd
Stefan Boonstra <stefanboonstra@hotmail.com>
2014-03-05 14:23:12 +0100

    Added Django project
    Removed generate data files

--- nrgweb_data/huishoudenimport.sql
+++
@@ -1,4 +0,0 @@
-.separator ";"
-.import adreslijst.csv huishouden
-insert into nrgweb_huishouden (straat,huisnummer,huisnummer_toevoeging,woonplaats,postcode,aantal_personen,email,telefoonnummer,password) select * from huishouden;
-drop table huishouden;
