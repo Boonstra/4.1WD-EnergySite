c20bb65072cac803c2f155e7daed5d137d76400c
Stefan Boonstra <stefanboonstra@hotmail.com>
2014-03-05 15:44:13 +0100

    Updated facilities

--- facilities/models.py
+++ facilities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Facilities(models.Model):
+    street = models.CharField(max_length=80)
+    street2 = models.CharField(max_length=80)
+    city = models.CharField(max_length=80)
+    zipcode = models.CharField(max_length=80)
+    inhabitants = models.IntegerField()
+    email = models.CharField(max_length=80)
+    phone_number = models.CharField(max_length=80)
+    password = models.CharField(max_length=256)
