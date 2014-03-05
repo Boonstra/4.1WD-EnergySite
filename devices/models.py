26bab0ec009b5516dbb152ad48757eaa0fc32d47
Stefan Boonstra <stefanboonstra92@gmail.com>
2014-03-05 19:15:57 +0100

    Fixed devices Category model

--- devices/models.py
+++ devices/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from facilities.models import Facility
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=80)
+
+
 class Device(models.Model):
     facilities = models.ManyToManyField(Facility)
+    category = models.ForeignKey(Category)
     brand = models.CharField(max_length=80)
     model = models.CharField(max_length=80)
-
-
-class Category(models.Model):
-    device = models.ForeignKey(Device)
-    category = models.CharField(max_length=80)
