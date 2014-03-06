06ae7af4457a8198e220ad420c689d81c3fbb5ba
Stefan Boonstra <stefanboonstra92@gmail.com>
2014-03-06 18:40:00 +0100

    Improved measurements model

--- measurements/models.py
+++ measurements/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from devices.models import Device
-
-
-class Measurement(models.Model):
-    device = models.ForeignKey(Device)
-    datetime = models.DateTimeField()
-    value = models.IntegerField()
+from facilities.models import Facility
 
 
 class Time(models.Model):
     time = models.CharField(max_length=80)
+
+
+class Measurement(models.Model):
+    facility = models.ForeignKey(Facility)
+    device = models.ForeignKey(Device)
+    time = models.ForeignKey(Time)
+    value = models.FloatField()
