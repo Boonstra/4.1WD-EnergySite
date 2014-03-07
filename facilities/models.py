e3e616836284cf45a239129f9e00ea0d2dbf6453
Epo123 <erik.poolman123@gmail.com>
2014-03-07 19:32:09 +0100

    working coupling between facility and user

--- facilities/models.py
+++ facilities/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Facility(models.Model):
+    users = models.ManyToManyField(User, related_name='facilities')
     street = models.CharField(max_length=80)
     street2 = models.CharField(max_length=80)
     city = models.CharField(max_length=80)
