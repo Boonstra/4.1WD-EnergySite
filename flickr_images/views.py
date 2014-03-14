0db2f422a1c2f240352421d03e7a38432cb591e6
Stefan Boonstra <stefanboonstra92@gmail.com>
2014-03-14 18:17:10 +0100

    The image API now returns detailed image data

--- flickr_images/views.py
+++ flickr_images/views.py
@@ -21,6 +21,7 @@ class ImagesViewSet(viewsets.ViewSet):
     def list(self, request):
         settings = FlickrImage.objects.all()[0]
         page = request.GET.get('page')
+        per_page =request.GET.get('page')
 
         if not settings:
             return JSONResponse({})
@@ -28,20 +29,50 @@ class ImagesViewSet(viewsets.ViewSet):
         if not page:
             page = 1
 
+        if not per_page:
+            per_page = 1
+
         # Search for images by tag on the Flickr REST API
-        response = requests.get('https://api.flickr.com/services/rest/', params={
+        search_response = requests.get('https://api.flickr.com/services/rest/', params={
             'tags': settings.tags,
             'page': page,
-            'per_page': '1',
+            'per_page': per_page,
 
             # Mandatory parameters
             'method': 'flickr.photos.search',
             'format': 'json',
             'api_key': '7a0a6d9d6aa08ea6360f33561aa56645',
-            'nojsoncallback': '1'
+            'nojsoncallback': '1',
         })
 
-        if response.status_code == 200:
-            return JSONResponse({'flickr_response': response.json(), 'refresh_rate': settings.refresh_rate})
+        if search_response.status_code != 200:
+            return JSONResponse({})
 
-        return JSONResponse({})
+        flickr_response = search_response.json()
+
+        if flickr_response['stat'] != 'ok':
+            return JSONResponse({})
+
+        images = flickr_response['photos']['photo']
+
+        detailed_images = []
+
+        # Get information about the returned photos
+        for image in images:
+            detailed_response = requests.get('https://api.flickr.com/services/rest/', params={
+                'photo_id': image['id'],
+                'secret': image['secret'],
+
+                # Mandatory parameters
+                'method': 'flickr.photos.getInfo',
+                'format': 'json',
+                'api_key': '7a0a6d9d6aa08ea6360f33561aa56645',
+                'nojsoncallback': '1',
+            })
+
+            if detailed_response.status_code != 200:
+                continue
+
+            detailed_images.append(detailed_response.json())
+
+        return JSONResponse({'images': detailed_images, 'refresh_rate': settings.refresh_rate})
